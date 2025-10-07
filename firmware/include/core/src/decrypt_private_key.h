f3d74d61206d9fd3fa8ac1cf91ee028bf47331aa
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2025-10-07 22:36:16 +0200

    feat: add multi-keys support

--- firmware/include/core/src/decrypt_private_key.h
+++ firmware/include/core/src/decrypt_private_key.h
@@ -2,7 +2,7 @@
 #include <mbedtls/aes.h>
 #include <input.h>
 
-void decrypt_private_key(uint8_t *key, uint8_t *output) {
+void decrypt_private_key(uint8_t *key, byte pos, uint8_t *output) {
     mbedtls_aes_context aes;
     mbedtls_aes_init(&aes);
 
@@ -13,11 +13,22 @@ void decrypt_private_key(uint8_t *key, uint8_t *output) {
     unsigned char iv[16] = AES_IV_INITIALIZER;
 
     // Set encrypted data
-    unsigned char encrypted_data[48] = ENCRYPTED_DATA_INITIALIZER;
+    int encrypted_data_len = 144;
+    unsigned char encrypted_data[encrypted_data_len] = ENCRYPTED_DATA_INITIALIZER;
 
+    int decrypted_data_len = 128;
+    unsigned char decrypted_data[decrypted_data_len] = {0};
     // Print decrypted data
-    mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, 48, iv, encrypted_data, output);
+    mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, encrypted_data_len, iv, encrypted_data, decrypted_data);
 
     // Clean up
     mbedtls_aes_free(&aes);
+
+    // Copy the relevant 32-byte segment to output
+    if (pos < 0 || pos > decrypted_data_len - 32) {
+        return;
+    }
+
+    memcpy(output, decrypted_data + pos * 32, 32);
+
 }
