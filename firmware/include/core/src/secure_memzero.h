93841b022fc2576bd68f39924cdcaec4cd929135
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2026-05-14 23:07:05 +0200

    fix: add compiler barrier to secure_memzero function

--- firmware/include/core/src/secure_memzero.h
+++ firmware/include/core/src/secure_memzero.h
@@ -4,10 +4,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
-void secure_memzero(volatile void* ptr, size_t len) {
-    if (!ptr) return;
-    volatile uint8_t* p = (volatile uint8_t*)ptr;
+void secure_memzero(void *ptr, size_t len) {
+    volatile uint8_t *p = (volatile uint8_t *)ptr;
     while (len--) *p++ = 0;
+    __asm__ volatile ("" : : "r"(ptr) : "memory");
 }
 
 #endif // SECURE_MEMZERO_H
